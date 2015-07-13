13f6a450c19b83996d32272c710b75da04c931f0
Martin Johnson <e356@2011-c2wgkyc6dv13.env.uea.ac.uk>
2015-07-13 17:55:14 +0100

    lots of hacking, still need to get particle numbering in spawn file right - over to you Matt

--- spawn_control.py
+++ spawn_control.py
@@ -1,32 +1,81 @@
-# Setting up spawn file
+# Spawn file setup for tracer release in two positions
 #
 #   
-#
+#this looks really handy http://www.unidata.ucar.edu/blogs/developer/en/entry/accessing_netcdf_data_by_coordinates
 
 
 import  csv
+from netCDF4 import Dataset
+import numpy as np
 
-p = range(1,50) #number of particles
-l = -6.536887   #latitude 
-#numpy.arange(-5, -6, 0.0005) code use later
-la = 59.244806  #Longitude
-#numpy.arange(58, 59, 0.0005) code use later
-d = 00.0000     #Depth
-da = 2015/10/01 #Date
-list_of_lists = [[p[0],l,la,d,da]]
+# define + open netcdf physical model output
+model_output = "north_west_european_shelf.3d.200110.3h.nc"
 
-with open("spawntest.csv","w") as f:     
-     wr = csv.writer(f,delimiter=" ")     
-     wr.writerows(list_of_lists)
-     wr.writerows('\n')
+dataset = Dataset(model_output,'r')
 
-counter = 0
-while counter <= 50:
-     p[0] = p[0] + 1
-     while d <= 50:
-          d = d + 1 
-          with open("spawntest.csv","w") as f:   
-               wr = csv.writer(f,delimiter=" ")     
-               wr.writerows(list_of_lists)
-               wr.writerows('\n')
-          counter = counter + 1
+p_box = 50 #number of particles per box
+
+date = "testdate"
+
+locations=[[59,-6],[56,2]]
+
+
+def brute_force_location_index(latvar,lonvar,lat0,lon0):
+    latvals = latvar[:]
+    lonvals = lonvar[:]
+    ny,nx = latvals.shape[0], lonvals.shape[0]
+    dist_sq_min = 1.0e30
+    for iy in range(ny):
+        for ix in range(nx):
+            latval = latvals[iy]
+            lonval = lonvals[ix]
+            dist_sq = (latval - lat0)**2 + (lonval - lon0)**2
+            if dist_sq < dist_sq_min:
+                iy_min, ix_min, dist_sq_min = iy, ix, dist_sq
+    return iy_min,ix_min
+
+
+
+
+#iy,ix = brute_force_location_index(latvar, lonvar, 59, -6)
+#print('Closest lat lon:', latvar[iy,ix], lonvar[iy,ix])
+
+
+def createspawnlist_singlelocation(nparticles, latitude, longitude, date, model_output):
+    
+    #distance from bottom of deepest particle realease
+    bottom_scrape = 0.1
+
+
+    p = list(range(nparticles)) #number of particles
+
+
+
+    latvar = model_output.variables['latc']
+    lonvar = model_output.variables['lonc']
+    
+    # Get netcdf x and y indexes for lon lat position
+    iy, ix = brute_force_location_index(latvar, lonvar, latitude, longitude)
+    
+    depth=dataset.variables['bathymetry'][iy,ix]
+    print depth
+    
+    depthinterval = (depth - bottom_scrape)/(nparticles-1)
+
+
+    #create list of lists to write out file - first particle is particle number, second is depth (needs improving)
+    list_of_lists = [[particle+1,latitude,longitude,(particle)*depthinterval,date] for particle in p]
+    return list_of_lists
+
+
+lists = [createspawnlist_singlelocation(p_box,location[0],location[1],date,dataset) for location in locations]
+
+list_of_lists=lists[0]+lists[1]
+
+with open("spawntest.csv","w") as f:
+    wr = csv.writer(f,delimiter=' ')
+    wr.writerows(list_of_lists)
+
+    f.close()
+
+
